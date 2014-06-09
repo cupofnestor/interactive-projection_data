946c548bbc219f235ad6ce17810a199ede63bc37
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-06-09 11:19:35 -0400

    First Commit!

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,16 +1,17 @@
-var ds = new Datasource("/Volumes/Pylos/Projects/FED/projection.db");
+var ds = new Datasource("/Data/projection.db");
 var defs;
 var datasource;
 ds.setup().then(
 	function(d){
 		defs = d;
 		$("body").html("<h1>Definitions:</h1><pre>"+JSON.stringify(defs,null,3)+"</pre>");
-		ds.get(defs.Category_1[0]).then(
+		/*ds.get(defs.Category_1[0]).then(
 			function(d){
 			data = d;
 			console.log("done");
-			makeMenu();
-		})
+			
+		})*/
+		makeMenu();
 	}
 		
 )
