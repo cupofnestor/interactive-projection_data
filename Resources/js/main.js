cbf371664ea97542aa9c592f7d1ecf497092c8df
W. Ryan Nestor <nestor@fuzzy-math.com>
2014-06-12 15:28:45 -0400

    updating definitions, works with world map, etc.

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -5,7 +5,7 @@ ds.setup().then(
 	function(d){
 		defs = d;
 		$("body").html("<h1>Definitions:</h1><pre>"+JSON.stringify(defs,null,3)+"</pre>");
-		ds.get(defs.Category_1[0]).then(
+		ds.get(defs.Category_3[1]).then(
 			function(d){
 			data = d;
 			console.log("done");
