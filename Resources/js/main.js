351dced7efa2fefb527911e240cbf0b114fe5292
Ray Ortgiesen <ray.ortgiesen@gmail.com>
2014-06-18 13:35:10 -0400

    Finished Accordian Menu Structure
    Clicking a chart now prints it’s __data__

--- Resources/js/main.js
+++ Resources/js/main.js
@@ -1,9 +1,16 @@
 var ds = new Datasource("/Data/projection.db");
 var defs;
 var datasource;
+var test;
+
 ds.setup().then(
 	function(d){
 		defs = d;
+		test = $.map(defs, function(value, index) {return [value]});
+		
+	
+		//returns object as array
+		
 		//$("body").html("<h1>Definitions:</h1><pre>"+JSON.stringify(defs,null,3)+"</pre>");
 		
 		/*ds.get(defs.Category_1[0]).then(
@@ -14,13 +21,33 @@ ds.setup().then(
 		})*/
 		makeMenu();
 	}
-		
 )
 
 makeMenu = function(){
-	var menu = d3.select("body").append("div");
-	var cat = menu.selectAll("div").data(Object.keys(defs)).enter().append("div")
-	cat.append("h2").text(function(d){return d})
+	var menu = d3.select("body").append("div").attr("id", "accordion");
+	var cat = menu.selectAll("div").data(Object.keys(defs)).enter().append("h3").text(function(d){return d});
 	
-	cat.selectAll("p").data(function(d){return defs[d]}).enter().append("p").text(function(d){return d.chart_name})
+	cat.append("div").attr("class", "accord-items")
+		.selectAll("p").data(function(d){return defs[d]}).enter().insert("p").text(function(d){return d.chart_name});
+	
+	
+	var items = $(".accord-items").detach();
+	var headers = $("h3");
+	
+	$.each( headers, function( i, val ) {
+		$(headers[i]).after(items[i]);
+	});
+	
+	
+	$( "p" ).on( "click", function() {
+		$("p").removeClass("selected");
+		$(this).addClass("selected");
+		console.log($(this).prop("__data__"));
+	});
+	
+	
+	$( "#accordion" ).accordion({
+		heightStyle: "content",
+		collapsible: true,
+	});
 }
